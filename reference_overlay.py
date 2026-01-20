a0408af2baa0a2ca8ab534eea3266d060512566c
Timothy Nakev <99489258+MyNamesTim@users.noreply.github.com>
2026-01-19 21:38:35 -0500

    Added dual-monitor support
    Extended range of application display to allow for images to be shown on non-primary monitors

--- reference_overlay.py
+++ reference_overlay.py
@@ -9,13 +9,24 @@ opsys = platform.system()
 if opsys == "Windows":
     username = os.environ.get('USERNAME')
     init_path = os.path.expanduser("~/Pictures")
+
+    import ctypes
+    user32 = ctypes.windll.user32
+    SM_XVIRTUALSCREEN = 76
+    SM_YVIRTUALSCREEN = 77
+    SM_CXVIRTUALSCREEN = 78
+    SM_CYVIRTUALSCREEN = 79
+    vx = user32.GetSystemMetrics(SM_XVIRTUALSCREEN)
+    vy = user32.GetSystemMetrics(SM_YVIRTUALSCREEN)
+    vw = user32.GetSystemMetrics(SM_CXVIRTUALSCREEN)
+    vh = user32.GetSystemMetrics(SM_CYVIRTUALSCREEN)
 elif opsys == "Linux":
     import getpass
     username = getpass.getuser()
     init_path = os.path.expanduser("~/Documents")
 elif opsys == "Darwin":
     username = os.getlogin()
-    init_path = os.path.expanduser("~/Documents")
+    init_path = os.path.expanduser("~/")
 else:
     print("Unsupported OS detected.")
     input("Press enter to exit...")
@@ -37,11 +48,11 @@ while True:
     scale = float(input("What scale would you like the image to be? 1.00 is the original image scale.\n"))
     if scale < 0.0 or scale > 1.0:
         scale_confirm = input(
-            "The scale that you have entered is abnormal and may lead to image reversing or a loss of detail (artifacts, pixelation, and blurring) due to upscaling.\n\n"
+            "The scale that you have entered is abnormal and may lead to a loss of detail (artifacts, pixelation, and blurring) due to upscaling. Negative numbers can cause crashes.\n\n"
             "Are you sure that you would like this scale? (Y/N)\n"
         ).lower()
         if scale_confirm == "n":
-            print("Okay, let's try again.")
+            print("What is the new scale that you would like?")
             continue
         else:
             print("Confirmed.")
@@ -50,9 +61,9 @@ while True:
         break
 
 while True:
-    alpha = float(input("Enter an opacity value from 0.00 - 1.00.\n"))
-    if alpha < 0.0 or alpha > 1.0:
-        print("The opacity value you entered is invalid. Please try again.")
+    alpha = float(input("What opacity would you like the image to be? (0.00 - 1.00)\n"))
+    if alpha < 0.0 or alpha > 1.00:
+        print("The opacity that you entered was invalid. Try again.\n")
         continue
     else:
         break
@@ -76,10 +87,25 @@ t = tkinter.Tk()
 t.overrideredirect(True)
 t.wm_attributes("-topmost", True)
 
-screen_w = t.winfo_screenwidth()
-screen_h = t.winfo_screenheight()
-t.geometry(f'{screen_w}x{screen_h}+0+0')
-t.attributes("-alpha", 0.01)
+if opsys == "Windows":
+    import ctypes
+    user32 = ctypes.windll.user32
+    SM_XVIRTUALSCREEN = 76
+    SM_YVIRTUALSCREEN = 77
+    SM_CXVIRTUALSCREEN = 78
+    SM_CYVIRTUALSCREEN = 79
+    vx = user32.GetSystemMetrics(SM_XVIRTUALSCREEN)
+    vy = user32.GetSystemMetrics(SM_YVIRTUALSCREEN)
+    screen_w = user32.GetSystemMetrics(SM_CXVIRTUALSCREEN)
+    screen_h = user32.GetSystemMetrics(SM_CYVIRTUALSCREEN)
+else:
+    vx = 0
+    vy = 0
+    screen_w = t.winfo_screenwidth()
+    screen_h = t.winfo_screenheight()
+
+t.geometry(f'{screen_w}x{screen_h}+{vx}+{vy}')
+t.attributes("-alpha", 0.05)
 
 canvas = tkinter.Canvas(t, width=w, height=h, highlightthickness=0)
 canvas.pack()
@@ -94,11 +120,8 @@ def setpos(event):
     x, y = event.x_root, event.y_root
     t.geometry(f'{w}x{h}+{x}+{y}')
     t.attributes("-alpha", alpha)
-    t.attributes("-topmost", True)
     t.unbind("<Button-1>")
     print(f'Reference positioned at {x}, {y}')
 
 t.bind("<Button-1>", setpos)
 t.mainloop()
-
-
